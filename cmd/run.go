9abb0e084aaae377eb5297a3e82b16681b111556
samiralajmovic <alajmovic.samir@gmail.com>
2020-02-01 21:19:08 +0100

    Add all projects flag for run command

--- cmd/run.go
+++ cmd/run.go
@@ -8,6 +8,7 @@ import (
 
 func runCmd() *cobra.Command {
 	var dryRun bool
+	var allProjects bool
 	var tags []string
 	var projects []string
 
@@ -18,18 +19,19 @@ func runCmd() *cobra.Command {
 		Run`,
 		Args: cobra.MinimumNArgs(1),
 		Run: func(cmd *cobra.Command, args []string) {
-			executeRun(args, dryRun, tags, projects)
+			executeRun(args, dryRun, allProjects, tags, projects)
 		},
 	}
 
 	cmd.Flags().BoolVar(&dryRun, "dry-run", false, "Print what will be ran for each project")
+	cmd.Flags().BoolVarP(&allProjects, "all", "a", false, "Specify all projects")
 	cmd.Flags().StringSliceVarP(&tags, "tags", "t", []string{}, "Specify tags")
 	cmd.Flags().StringSliceVarP(&projects, "projects", "p", []string{}, "Specify project")
 
 	return &cmd
 }
 
-func executeRun(args []string, dryRunFlag bool, tagsFlag []string, projectsFlag []string) {
+func executeRun(args []string, dryRunFlag bool, allProjectsFlag bool, tagsFlag []string, projectsFlag []string) {
 	config := core.ReadConfig()
 
 	command, err := core.GetCommand(args[0], config.Commands)
@@ -40,25 +42,30 @@ func executeRun(args []string, dryRunFlag bool, tagsFlag []string, projectsFlag 
 		return
 	}
 
-	var tagProjects []core.Project
-	if len(tagsFlag) > 0 {
-		tagProjects, err = core.GetProjectsByTag(tagsFlag, config.Projects)
-		if err != nil {
-			fmt.Println(err)
-			return
+	var finalProjects []core.Project
+	if allProjectsFlag {
+		finalProjects = config.Projects
+	} else {
+		var tagProjects []core.Project
+		if len(tagsFlag) > 0 {
+			tagProjects, err = core.GetProjectsByTag(tagsFlag, config.Projects)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
 		}
-	}
 
-	var projects []core.Project
-	if len(projectsFlag) > 0 {
-		projects, err = core.GetProjects(projectsFlag, config.Projects)
-		if err != nil {
-			fmt.Println(err)
-			return
+		var projects []core.Project
+		if len(projectsFlag) > 0 {
+			projects, err = core.GetProjects(projectsFlag, config.Projects)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
 		}
-	}
 
-	finalProjects := core.GetUnionProjects(tagProjects, projects)
+		finalProjects = core.GetUnionProjects(tagProjects, projects)
+	}
 
 	core.PrintCommand(command)
 	for _, project := range finalProjects {
