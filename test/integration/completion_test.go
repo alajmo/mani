98415bf7bd143f46f6520becd51d2a324c670d13
samiralajmovic <alajmovic.samir@gmail.com>
2021-06-06 14:45:43 +0200

    wip

--- test/integration/completion_test.go
+++ test/integration/completion_test.go
@@ -8,26 +8,34 @@ var completionTests = []TemplateTest{
 	{
 		TestName:   "Print bash completion",
 		InputFiles: []string{},
-		TestCmd:    "$MANI completion sh",
-		Golden:     "completion/sh",
+		TestCmd:    "$MANI completion bash",
+		Golden:     "completion/bash",
 		WantErr:    false,
 	},
 
-	// {
-	// 	TestName: "Print zsh completion",
-	// 	InputFiles: []string { "" },
-	// 	TestCmd: "$MANI completion zsh",
-	// 	Golden: "completion/zsh",
-	// 	WantErr: false,
-	// },
+	{
+		TestName: "Print zsh completion",
+		InputFiles: []string { "" },
+		TestCmd: "$MANI completion zsh",
+		Golden: "completion/zsh",
+		WantErr: false,
+	},
 
-	// {
-	// 	TestName: "Print powershell completion",
-	// 	InputFiles: []string { "" },
-	// 	TestCmd: "$MANI completion psh",
-	// 	Golden: "completion/psh",
-	// 	WantErr: false,
-	// },
+	{
+		TestName: "Print fish completion",
+		InputFiles: []string { "" },
+		TestCmd: "$MANI completion fish",
+		Golden: "completion/fish",
+		WantErr: false,
+	},
+
+	{
+		TestName: "Print powershell completion",
+		InputFiles: []string { "" },
+		TestCmd: "$MANI completion psh",
+		Golden: "completion/psh",
+		WantErr: false,
+	},
 }
 
 func TestCompletionCmd(t *testing.T) {
