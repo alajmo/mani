8b97299db27a872392dfc02e35b0baa7357500ef
samiralajmovic <alajmovic.samir@gmail.com>
2024-10-22 22:45:47 +0200

    Working

--- core/print/print_block.go
+++ core/print/print_block.go
@@ -9,9 +9,18 @@ import (
 	"github.com/alajmo/mani/core/dao"
 )
 
+var PURPLE = "#9068BD"
 var GREEN = "#b5bd68"
 var BLUE = "#94B8D8"
 var RED = "#cc6666"
+var WHITE = "#E0E0E0"
+
+var KEY_COLOR = BLUE
+var VALUE_COLOR = WHITE
+var TRUE_COLOR = GREEN
+var FALSE_COLOR = RED
+var CMD_COLOR = PURPLE
+var ENV_COLOR = PURPLE
 
 func PrintProjectBlocks(projects []dao.Project, colorize bool) string {
 	if len(projects) == 0 {
@@ -23,18 +32,18 @@ func PrintProjectBlocks(projects []dao.Project, colorize bool) string {
 	output += fmt.Sprintln()
 
 	for i, project := range projects {
-		output += printKeyValue("", "Name", project.Name, ":", colorize, GREEN, false)
+		output += printKeyValue("", "Name", project.Name, ":", colorize, KEY_COLOR, false)
 		if project.Desc != "" {
-			output += printKeyValue("", "Description", project.Desc, ":", colorize, GREEN, false)
+			output += printKeyValue("", "Description", project.Desc, ":", colorize, KEY_COLOR, false)
 		}
 		if project.RelPath != project.Name {
-			output += printKeyValue("", "Path", project.RelPath, ":", colorize, GREEN, false)
+			output += printKeyValue("", "Path", project.RelPath, ":", colorize, KEY_COLOR, false)
 		}
-		output += printKeyValue("", "Url", project.Url, ":", colorize, GREEN, false)
-		output += printKeyValue("", "Sync", strconv.FormatBool(project.IsSync()), ":", colorize, GREEN, false)
+		output += printKeyValue("", "Url", project.Url, ":", colorize, KEY_COLOR, false)
+		output += printKeyValue("", "Sync", strconv.FormatBool(project.IsSync()), ":", colorize, KEY_COLOR, false)
 
 		if len(project.Tags) > 0 {
-			output += printKeyValue("", "Tags", project.GetValue("Tag", 0), ":", colorize, GREEN, false)
+			output += printKeyValue("", "Tags", project.GetValue("Tag", 0), ":", colorize, KEY_COLOR, false)
 		}
 
 		if len(project.EnvList) > 0 {
@@ -60,42 +69,42 @@ func PrintTaskBlock(tasks []dao.Task, colorize bool) string {
 	output += fmt.Sprintln()
 
 	for i, task := range tasks {
-		output += printKeyValue("", "Name", task.Name, ":", colorize, GREEN, false)
-		output += printKeyValue("", "Description", task.Desc, ":", colorize, GREEN, false)
-		output += printKeyValue("", "Theme", task.ThemeData.Name, ":", colorize, GREEN, false)
-		output += printKeyValue("", "Target", "", ":", colorize, GREEN, false)
-		output += printKeyValue("", "All", strconv.FormatBool(task.TargetData.All), ":", colorize, GREEN, true)
-		output += printKeyValue("", "Cwd", strconv.FormatBool(task.TargetData.Cwd), ":", colorize, GREEN, true)
-		output += printKeyValue("", "Projects", strings.Join(task.TargetData.Projects, ", "), ":", colorize, GREEN, true)
-		output += printKeyValue("", "Paths", strings.Join(task.TargetData.Paths, ", "), ":", colorize, GREEN, true)
-		output += printKeyValue("", "Tags", strings.Join(task.TargetData.Tags, ", "), ":", colorize, GREEN, true)
+		output += printKeyValue("", "Name", task.Name, ":", colorize, KEY_COLOR, false)
+		output += printKeyValue("", "Description", task.Desc, ":", colorize, KEY_COLOR, false)
+		output += printKeyValue("", "Theme", task.ThemeData.Name, ":", colorize, KEY_COLOR, false)
+		output += printKeyValue("", "Target", "", ":", colorize, KEY_COLOR, false)
+		output += printKeyValue("", "All", strconv.FormatBool(task.TargetData.All), ":", colorize, KEY_COLOR, true)
+		output += printKeyValue("", "Cwd", strconv.FormatBool(task.TargetData.Cwd), ":", colorize, KEY_COLOR, true)
+		output += printKeyValue("", "Projects", strings.Join(task.TargetData.Projects, ", "), ":", colorize, KEY_COLOR, true)
+		output += printKeyValue("", "Paths", strings.Join(task.TargetData.Paths, ", "), ":", colorize, KEY_COLOR, true)
+		output += printKeyValue("", "Tags", strings.Join(task.TargetData.Tags, ", "), ":", colorize, KEY_COLOR, true)
 
-		output += printKeyValue("", "Spec", "", ":", colorize, GREEN, false)
-		output += printKeyValue("", "Output", task.SpecData.Output, ":", colorize, GREEN, true)
-		output += printKeyValue("", "Parallel", strconv.FormatBool(task.SpecData.Parallel), ":", colorize, GREEN, true)
-		output += printKeyValue("", "IgnoreErrors", strconv.FormatBool(task.SpecData.IgnoreErrors), ":", colorize, GREEN, true)
-		output += printKeyValue("", "OmitEmpty", strconv.FormatBool(task.SpecData.OmitEmpty), ":", colorize, GREEN, true)
+		output += printKeyValue("", "Spec", "", ":", colorize, KEY_COLOR, false)
+		output += printKeyValue("", "Output", task.SpecData.Output, ":", colorize, KEY_COLOR, true)
+		output += printKeyValue("", "Parallel", strconv.FormatBool(task.SpecData.Parallel), ":", colorize, KEY_COLOR, true)
+		output += printKeyValue("", "IgnoreErrors", strconv.FormatBool(task.SpecData.IgnoreErrors), ":", colorize, KEY_COLOR, true)
+		output += printKeyValue("", "OmitEmpty", strconv.FormatBool(task.SpecData.OmitEmpty), ":", colorize, KEY_COLOR, true)
 
 		if len(task.EnvList) > 0 {
 			output += printEnv(task.EnvList, colorize)
 		}
 
 		if task.Cmd != "" {
-			output += printKeyValue("", "Cmd", "", ":", colorize, GREEN, false)
+			output += printKeyValue("", "Cmd", "", ":", colorize, KEY_COLOR, false)
 			output += printCmd(task.Cmd)
 		}
 
 		if len(task.Commands) > 0 {
-			output += printKeyValue("", "Commands", "", ":", colorize, GREEN, false)
+			output += printKeyValue("", "Commands", "", ":", colorize, KEY_COLOR, false)
 			for _, subCommand := range task.Commands {
 				if subCommand.Name != "" {
 					if subCommand.Desc != "" {
-						output += printKeyValue("- ", subCommand.Name, subCommand.Desc, ":", colorize, BLUE, true)
+						output += printKeyValue("- ", subCommand.Name, subCommand.Desc, ":", colorize, CMD_COLOR, true)
 					} else {
-						output += printKeyValue("- ", subCommand.Name, "", "", colorize, BLUE, true)
+						output += printKeyValue("- ", subCommand.Name, "", "", colorize, CMD_COLOR, true)
 					}
 				} else {
-					output += printKeyValue("- ", "cmd", "", "", colorize, BLUE, true)
+					output += printKeyValue("- ", "cmd", "", "", colorize, CMD_COLOR, true)
 				}
 			}
 		}
@@ -111,15 +120,15 @@ func PrintTaskBlock(tasks []dao.Task, colorize bool) string {
 
 func printKeyValue(prefix string, key string, value string, seperator string, colorize bool, color string, padding bool) string {
 	str := ""
-	valueColor := "-"
+	valueColor := VALUE_COLOR
 	if value == "true" {
-		valueColor = GREEN
+		valueColor = TRUE_COLOR
 	} else if value == "false" {
-		valueColor = RED
+		valueColor = FALSE_COLOR
 	}
 
 	if colorize {
-		str = fmt.Sprintf("%s[%s:b]%s[-::-]%s [%s::-]%s\n", prefix, "white", key, seperator, valueColor, value)
+		str = fmt.Sprintf("%s[%s:b]%s[-:-]%s [%s::-]%s\n", prefix, color, key, seperator, valueColor, value)
 	} else {
 		str = fmt.Sprintf("%s%s: %s\n", prefix, key, seperator, value)
 	}
@@ -144,11 +153,11 @@ func printCmd(cmd string) string {
 func printEnv(env []string, colorize bool) string {
 	output := ""
 
-	output += printKeyValue("", "Env", "", ":", colorize, GREEN, false)
+	output += printKeyValue("", "Env", "", ":", colorize, KEY_COLOR, false)
 
 	for _, env := range env {
 		parts := strings.SplitN(strings.TrimSuffix(env, "\n"), "=", 2)
-		output += printKeyValue("", parts[0], parts[1], ":", colorize, BLUE, true)
+		output += printKeyValue("", parts[0], parts[1], ":", colorize, ENV_COLOR, true)
 	}
 
 	return output
